85109cebc9fcfbdcb4e39ce5393c4efed117f9d6
shubhamCSM1036 <2026csm1036@iitrpr.ac.in>
2026-08-05 23:39:17 +0530

    Implemented CSR graph representation

--- Assignment1/include/csr.h
+++ Assignment1/include/csr.h
@@ -1,8 +1,11 @@
 #ifndef CSR_H
 #define CSR_H
 
+#include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -13,16 +16,12 @@ struct CSRGraph
 
     vector<int> row_ptr;
     vector<int> col_idx;
+    vector<int> values;
 };
 
-CSRGraph readGraph(
-    const string& filename
-);
+CSRGraph readGraph(const string& filename);
 
-void printCSR(
-    const CSRGraph& graph
-);
-
+void printCSR(const CSRGraph& graph);
 
 void runCSR();
 
--- Assignment1/src/csr.cpp
+++ Assignment1/src/csr.cpp
@@ -1,10 +1,82 @@
 #include "../include/csr.h"
 
-#include <iostream>
-
 using namespace std;
+
+CSRGraph readGraph(const string& filename)
+{
+    ifstream file(filename);
+
+    if (!file)
+    {
+        cerr << "Error opening file.\n";
+        exit(1);
+    }
+
+    CSRGraph graph;
+
+    file >> graph.vertices >> graph.edges;
+
+    graph.row_ptr.push_back(0);
+
+    int current_index = 0;
+
+    for (int i = 0; i < graph.vertices; i++)
+    {
+        int vertex;
+        int degree;
+
+        file >> vertex >> degree;
+
+        for (int j = 0; j < degree; j++)
+        {
+            int neighbour;
+            int weight;
+
+            file >> neighbour >> weight;
+
+            graph.col_idx.push_back(neighbour);
+            graph.values.push_back(weight);
+
+            current_index++;
+        }
+
+        graph.row_ptr.push_back(current_index);
+    }
+
+    file.close();
+
+    return graph;
+}
+
+void printCSR(const CSRGraph& graph)
+{
+    cout << "\nRow Pointer:\n";
+
+    for (int value : graph.row_ptr)
+    {
+        cout << value << " ";
+    }
+
+    cout << "\n\nColumn Indices:\n";
+
+    for (int value : graph.col_idx)
+    {
+        cout << value << " ";
+    }
+
+    cout << "\n\nValues:\n";
+
+    for (int value : graph.values)
+    {
+        cout << value << " ";
+    }
+
+    cout << "\n";
+}
 
 void runCSR()
 {
-    cout << "CSR implementation is not complete yet." << endl;
+    CSRGraph graph = readGraph("tests/csr/csr_input.txt");
+
+    printCSR(graph);
 }
